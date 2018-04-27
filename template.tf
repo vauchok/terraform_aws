5f80239deaaed4f5592894df5e1ee3ecc132b912
Ihar Vauchok <31825372+vauchok@users.noreply.github.com>
2018-04-27 11:47:27 +0300

    create template.tf

---
+++ template.tf
@@ -0,0 +1,56 @@
+# Provider configuration
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_vpc" "test_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "test_public" {
+  vpc_id     = "${aws_vpc.test_vpc.id}"
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "allow_http" {
+  name = "allow_http"
+  description = "Allow HTTP traffic"
+  vpc_id = "${aws_vpc.test_vpc.id}"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "master-instance" {
+  ami           = "ami-78485818"
+  instance_type = "t2.micro"
+  subnet_id     = "${aws_subnet.test_public.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
+}
+
+resource "aws_instance" "slave-instance" {
+  ami           = "ami-78485818"
+  instance_type = "t2.micro"
+  subnet_id     = "${aws_subnet.test_public.id}"
+  depends_on    = ["aws_instance.master-instance"]
+  vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
+/*
+  tags {
+    master_hostname = "${aws_instance.master-instance.private_dns}"
+  }
+*/
+  lifecycle {
+    ignore_changes = ["tags"]
+  }
+}
