dfc70a9bbcb3b27d605971b1519634d23b66b932
Ihar Vauchok <31825372+vauchok@users.noreply.github.com>
2018-05-24 17:36:26 +0300

    change cred

--- template.tf
+++ template.tf
@@ -1,6 +1,8 @@
 # Provider configuration
 provider "aws" {
-  region = "us-west-1"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "${var.aws_region}"
 }
 
 resource "aws_vpc" "test_vpc" {
